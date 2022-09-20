abf8dd381e98467835a54c30ff886d5d8f469777
talhayavcin <talhayavcin35@gmail.com>
2022-09-20 17:01:56 +0300

    added filter function

--- src/layout/Sidebar.js
+++ src/layout/Sidebar.js
@@ -1,10 +1,15 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable jsx-a11y/anchor-has-content */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState } from "react";
 import { HomeIcon, SearchIcon, LibraryIcon, VectorIcon, ButtonSearchIcon} from "../icons/Icons";
 import Logo from '../images/Vector.png';
+import { allPlaylists } from "./users";
 
 const Sidebar = () => {
+
+  const [searchMusic, setSearchMusic] = useState("");
   return (
     <div className="h-screen sticky top-0 flex flex-col justify-between w-56  bg-black-black">
       <div>
@@ -46,12 +51,23 @@ const Sidebar = () => {
           <label for="search-input" className="text-black w-12 h-10 flex items-center justify-center absolute top-0 left-0">
             <ButtonSearchIcon/>
           </label>
-          <input type="text" id="search-input" className="h-10 pl-12 outline-none text-white font-small bg-white bg-opacity-20 rounded-3xl text-sm placeholder-black/50 w-40" placeholder="Ara"/>
+          <input type="text" id="search-input" className="h-10 pl-12 outline-none text-white font-small bg-white bg-opacity-20 rounded-3xl text-sm placeholder-black/50 w-40" placeholder="Ara" onChange={(event) => {setSearchMusic(event.target.value)}}/>
           <label className="text-black w-12 h-10 flex items-center  justify-end absolute top-0 ml-24">
             <VectorIcon/>
           </label>
         </div>
       </div>
+      <nav className="mx-6 mt-2 py-2 flex-auto overflow-auto">
+        <ul>
+        {allPlaylists
+          .filter((asd) => asd.name.toLowerCase().includes(searchMusic))
+          .map((user) => (
+            <li className="listItem" key={user.id}>
+              <a className="text-sm text-link flex h-8 items-center text-white text-opacity-60">{user.name}</a>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </div>
   )
 }
